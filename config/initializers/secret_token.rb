1bab07ca4c0df2cf2c93fecd0cbd00369e16e9e8
David Kormushoff <kormie@gmail.com>
2012-02-17 01:18:20 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twstudygroup::Application.config.secret_token = '822e8e251eb8055e296ad5c317003be280f5754c2da272476f56247ad5eeff52fcc90d835893ddf921629479fcd84ec13874c476df1f419a063ab7c4914b7d1a'
